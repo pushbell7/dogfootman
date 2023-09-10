6ba2ca1bde0ceb4f6592fa656d304a335ae2d5dc
pushbell7 <redjm900@gmail.com>
2023-09-10 18:54:19 +0900

    implement traffic control

--- DogFootMan/Assets/Scripts/RoadInfo.cs
+++ DogFootMan/Assets/Scripts/RoadInfo.cs
@@ -23,7 +23,17 @@ public class RoadInfo : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+    }
+    void OnDrawGizmos()
+    {
+        //Gizmos.color = Color.red;
+
+        //var collider = GetComponent<BoxCollider>();
+        //Vector3 size = new Vector3(collider.size.x * transform.localScale.x, collider.size.y * transform.localScale.y, collider.size.z * transform.localScale.z);
+        //Vector3 position = transform.position;
+        //Quaternion rotation = transform.rotation;
+        //Gizmos.matrix = Matrix4x4.TRS(position, rotation, size);
+        //Gizmos.DrawWireCube(Vector3.zero, Vector3.one);
     }
 
     public Vector3 GetCenterOfLanePosition(int lane, Vector3 currentPositionOfRunningObject)
@@ -68,6 +78,20 @@ public class RoadInfo : MonoBehaviour
         return destination;
     }
 
+    public Vector3 GetStartingPointOfLane(int lane)
+    {
+        var boxCollider = GetComponent<BoxCollider>();
+        var halfSizeZ = boxCollider.size.z * gameObject.transform.localScale.z / 2;
+        float laneWidth = boxCollider.size.x * gameObject.transform.localScale.x / (ForwardLaneCount + BackwardLaneCount);
+        float centerOfLane = laneWidth * lane - (lane > 0 ? 1 : -1) * laneWidth / 2;
+        Vector2 destination = new Vector2(centerOfLane, lane > 0 ? -halfSizeZ : halfSizeZ);
+
+        destination = Quaternion.Euler(0f, 0f, -gameObject.transform.rotation.eulerAngles.y) * destination;
+        destination += new Vector2(gameObject.transform.position.x, gameObject.transform.position.z);
+
+        return new Vector3(destination.x, 20f, destination.y);
+    }
+
     public int GetTheNumberOfLane(Vector3 currentPositionOfRunningObject)
     {
         currentPositionOfRunningObject -= gameObject.transform.position;
@@ -90,10 +114,15 @@ public class RoadInfo : MonoBehaviour
         float xScale = boxCollider.size.x * transform.localScale.x / 2;
         float zScale = boxCollider.size.z * transform.localScale.z / 2;
 
-        return IsInRotatedRectangle(new Vector2(positionToCheck.x, positionToCheck.z), new Vector2(center.x, center.z), new Vector2(xScale, zScale), transform.rotation.eulerAngles.y);
+        return IsInRotatedRectangle(positionToCheck, center, new Vector3(xScale, 0, zScale), transform.rotation.eulerAngles.y);
     }
 
-    bool IsInRotatedRectangle(Vector2 point, Vector2 rectanglePosition, Vector2 halfRectangleSize, float rectangleRotation)
+    static public bool IsInRotatedRectangle(Vector3 point, Vector3 rectanglePosition, Vector3 halfRectangleSize, float rectangleRotation)
+    {
+        return IsInRotatedRectangle(new Vector2(point.x, point.z), new Vector2(rectanglePosition.x, rectanglePosition.z), new Vector2(halfRectangleSize.x, halfRectangleSize.z), rectangleRotation);
+    }
+
+    static bool IsInRotatedRectangle(Vector2 point, Vector2 rectanglePosition, Vector2 halfRectangleSize, float rectangleRotation)
     {
         // Translate the point to the rectangle's local coordinate space
         Vector2 center = rectanglePosition;
--- DogFootMan/Assets/Scripts/TrafficControlTrigger.cs
+++ DogFootMan/Assets/Scripts/TrafficControlTrigger.cs
@@ -8,16 +8,69 @@ public class TrafficControlTrigger : MonoBehaviour
 
     enum TrafficState
     {
-        GO,
-        STOP,
+        Horizontal,
+        Vertical,
         MAX
     }
     private TrafficState CurrentTrafficState;
+    private List<GameObject> ConnectedRoads;
+    private Dictionary<GameObject, List<Vector3>> CandidatePointMap;
 
     // Start is called before the first frame update
     void Start()
     {
         WaitingObjectUnderControl = new List<GameObject>();
+
+        // get connected roads in this volume
+        ConnectedRoads = new List<GameObject>();
+        CandidatePointMap = new Dictionary<GameObject, List<Vector3>>();
+
+        var collider = GetComponent<BoxCollider>();
+        Vector3 size = new Vector3(collider.size.x * transform.localScale.x, collider.size.y * transform.localScale.y, collider.size.z * transform.localScale.z);
+        Collider[] colliders = Physics.OverlapBox(transform.position, size / 2, transform.rotation);
+
+        foreach(var selectedCollider in colliders)
+        {
+            if (selectedCollider.CompareTag("Road"))
+            {
+                ConnectedRoads.Add(selectedCollider.gameObject);
+            }
+        }
+
+        // get every lane point
+        foreach(var road in ConnectedRoads)
+        {
+            var candidatePoint = new List<Vector3>();
+            var roadInfo = road.GetComponent<RoadInfo>();
+            for(int i = 1; i <= roadInfo.ForwardLaneCount; ++i)
+            {
+                if (RoadInfo.IsInRotatedRectangle(roadInfo.GetStartingPointOfLane(i), transform.position, size / 2, transform.rotation.eulerAngles.y))
+                {
+                    candidatePoint.Add(roadInfo.GetStartingPointOfLane(i));
+                }
+            }
+            for(int i = -1; i >= -roadInfo.BackwardLaneCount; --i)
+            {
+                if (RoadInfo.IsInRotatedRectangle(roadInfo.GetStartingPointOfLane(i), transform.position, size / 2, transform.rotation.eulerAngles.y))
+                {
+                    candidatePoint.Add(roadInfo.GetStartingPointOfLane(i));
+                }
+            }
+
+            CandidatePointMap.Add(road, candidatePoint);
+        }
+
+    }
+    void OnDrawGizmos()
+    {
+        //Gizmos.color = Color.blue;
+
+        //var collider = GetComponent<BoxCollider>();
+        //Vector3 size = new Vector3(collider.size.x * transform.localScale.x, collider.size.y * transform.localScale.y, collider.size.z * transform.localScale.z);
+        //Vector3 position = transform.position;
+        //Quaternion rotation = transform.rotation;
+        //Gizmos.matrix = Matrix4x4.TRS(position, rotation, size);
+        //Gizmos.DrawWireCube(Vector3.zero, Vector3.one);
     }
 
     private void Update()
@@ -27,10 +80,10 @@ public class TrafficControlTrigger : MonoBehaviour
 
         switch(CurrentTrafficState)
         {
-            case TrafficState.GO:
+            case TrafficState.Horizontal:
                 WaitingObjectUnderControl.ForEach(obj => obj.GetComponent<CarController>()?.SetWait(false));
                 break;
-            case TrafficState.STOP:
+            case TrafficState.Vertical:
                 WaitingObjectUnderControl.ForEach(obj => obj.GetComponent<CarController>()?.SetWait(true));
                 break;
             default: break;
@@ -40,10 +93,24 @@ public class TrafficControlTrigger : MonoBehaviour
     private void OnTriggerEnter(Collider other)
     {
         WaitingObjectUnderControl.Add(other.gameObject);
+
+        MakeDestination(other.gameObject);
     }
 
     private void OnTriggerExit(Collider other)
     {
         WaitingObjectUnderControl.Remove(other.gameObject);
+
+        other.gameObject.GetComponent<CarController>()?.SetWait(false);
+    }
+
+    void MakeDestination(GameObject InObject)
+    {
+        // @TODO: flow control 
+        // 1. define connected road count
+        //ConnectedRoads.Count;
+        // 2. which road is InObject on?
+        // 3. which lane does InObject use?
+        // 4. find road to use and decide point from CandidatePointMap
     }
 }
